8a5a4b36336dec1c569be9b4b7de6f4248000c8e
greatorangejuice <bypavelsnigirev@gmail.com>
2019-02-28 00:15:29 +0300

    fix: - fix function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,37 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  let index = preferences.length;
+
+  for (let i = 0; i < index; i++) {
+    let m = preferences[i]; // 2
+    let n = preferences[m-1]; // 3
+    let k = preferences[n-1]; // 1
+
+    if ( m-1 == i && n == i ) {
+      count++
+    }
+
+    // if ( preferences[] == )
+
+    if ( preferences[k-1] == m && preferences[n-1] == )
+    // Сравниваю значение с индексом. То есть число K == индексу M. Значение M == индексу N. 
+    // Значение N == индексу K.
+  }
+  return count;
 };
+// indexOf!!!
+
+m  n  k
+2, 3, 1, 5, 6, 4
+
+1  2  3  4  5  6
+
+
+// Если индекс двойки равен значению единицы ...
+
+
+
+//  индекс: n k m
+//значение: k m n
